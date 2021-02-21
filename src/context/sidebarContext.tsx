1e08d012bc12ef845ee68fe06eea7299e3fc6628
bratishques <gurda999@gmail.com>
2021-02-21 19:33:49 +0300

    Resp. tinkering

--- src/context/sidebarContext.tsx
+++ src/context/sidebarContext.tsx
@@ -23,8 +23,8 @@ const SidebarContextProvider = ({ children }: Props) => {
 
   useEffect(() => {
     const appHeight = () => {
-      const doc = document.documentElement
-      doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
       window.addEventListener('resize', appHeight)
       appHeight()

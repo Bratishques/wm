0109dac5737206f85bd0ac86d02517f902309df9
bratishques <gurda999@gmail.com>
2021-02-21 19:11:59 +0300

    Tailwind tinkering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app';
-import "tailwindcss/tailwind.css";
-import '@/styles/global.css';
+import '../styles/global.css';
 import { SidebarContextProvider } from '@/context/sidebarContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
